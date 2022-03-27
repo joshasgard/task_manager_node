fb9ca5d751ab7cd523248727a00968001693b0cb
joshasgard <idowuodesanmi@gmail.com>
2022-03-27 18:21:43 +0100

    feat: Setup MongoDB connection and add environment variables

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
-const express = require('express');
+require("./db/connect");
+const express = require("express");
 const app = express();
-const tasks = require('./routes/tasks')
+const tasks = require("./routes/tasks");
+const connectDB = require("./db/connect");
+require("dotenv").config();
 
 // middleware
 
-app.use(express.json())
+app.use(express.json());
 
 // routes
 
-app.get('/home', (req, res)=>{
-    res.send('<h1>Task Manager App</h1>')
-})
+app.get("/home", (req, res) => {
+  res.send("<h1>Task Manager App</h1>");
+});
 
-app.use('/api/v1/tasks', tasks)
+app.use("/api/v1/tasks", tasks);
 // Routes to create
 //app.get('/api/v1/tasks') - get all the tasks
 //app.post('/api/v1/tasks') - create a new task
@@ -20,7 +23,15 @@ app.use('/api/v1/tasks', tasks)
 //app.patch('/api/v1/tasks') - update task
 //app.delete('/api/v1/tasks') - delete task
 
+const port = 3000;
 
-const port = 3000
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`Server is listening on port ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-app.listen(port, console.log(`Server is listening on port ${port}...`))
+start();
